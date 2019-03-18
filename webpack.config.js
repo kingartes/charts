956f54df585ab91ebf524612fc2d85a5dd68bfb5
KingArtes <kingartes1989@gmail.com>
2019-03-18 22:09:48 +0200

    Heroku procfile

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9000
+        port: process.env.PORT
     },
     devtool: 'eval-source-map'
 };
