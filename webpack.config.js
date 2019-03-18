6d9d0d36b6fd7de7e8adcbc43658d924e6ec83b8
KingArtes <kingartes1989@gmail.com>
2019-03-18 22:39:33 +0200

    Heroku procfile

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: process.env.PORT,
+        port: 9000,
         headers: {
             "Access-Control-Allow-Origin": "*"
         }
